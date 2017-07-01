54a878753d2c20abc866f0c3a52f02afb22c8a7b
Phi Anh <tranhoangphianh01@gmail.com>
2017-07-01 14:59:44 +0000

    Upload New File

---
+++ GraphicProject/Shape.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace GraphicProject
+{
+    abstract class Shape
+    {
+        Color color;
+        TypeDraw typeDraw;
+
+        protected Shape()
+        {
+
+        }
+
+        protected Shape(TypeDraw typeDraw)
+        {
+            this.typeDraw = typeDraw;
+            this.color = Color.Black; //default color
+        }
+
+        protected void setTypeDraw(TypeDraw typeDraw)
+        {
+            this.typeDraw = typeDraw;
+        }
+
+        public void setColor(Color color)
+        {
+            this.color = color;
+        }
+
+        public Color getColor()
+        {
+            return color;
+        }
+
+        public TypeDraw getTypeDraw()
+        {
+            return typeDraw;
+        }
+    }
+}
