52bd1b7ca4d0932870daff2081df85a909131636
Phi Anh <tranhoangphianh01@gmail.com>
2017-07-01 14:57:26 +0000

    Upload New File

---
+++ GraphicProject/Line.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace GraphicProject
+{
+    class Line:Shape
+    {
+        private Point startPoint, endPoint;
+        public Line(Point startPoint,Point endPoint,Color color):this(startPoint,endPoint)
+        {
+            setColor(color);
+        }
+
+        public Line(Point startPoint,Point endPoint): base(TypeDraw.Line)
+        {
+            this.startPoint = startPoint;
+            this.endPoint = endPoint;
+        }
+
+        public Line():this(new Point(0, 0), new Point(0, 0))
+        {
+        }
+
+        public void setStartPoint(Point startPoint)
+        {
+            this.startPoint = startPoint;
+        }
+
+        public void setEndPoint(Point endPoint)
+        {
+            this.endPoint = endPoint;
+        }
+
+        public Point getStartPoint()
+        {
+            return startPoint;
+        }
+        
+        public Point getEndPoint()
+        {
+            return endPoint;
+        }
+
+    }
+}
