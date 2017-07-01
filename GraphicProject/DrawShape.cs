d0ebf5d1522ec6f682360665f5d15cea12f8bc10
Phi Anh <tranhoangphianh01@gmail.com>
2017-07-01 14:55:02 +0000

    Upload New File

---
+++ GraphicProject/DrawShape.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicProject
+{
+    class DrawShape
+    {
+        public static int round(float num)
+        {
+            //handle
+            return 1;
+        }
+
+        public static void drawDDA()
+        {
+
+        }
+
+        public static void draw(Shape shape)
+        {
+            switch (shape.getTypeDraw())
+            {
+                case TypeDraw.Line:
+
+                    break;
+                case TypeDraw.Circle:
+
+                    break;
+                case TypeDraw.Ellipse:
+
+                    break;
+                case TypeDraw.Rectangle:
+
+                    break;
+                case TypeDraw.Square:
+
+                    break;
+                case TypeDraw.Triangle:
+
+                    break;
+                case TypeDraw.Parallelogram:
+
+                    break;
+            }
+        }
+
+    }
+}
