152011591287cc74871067dbfc00130c4e90d1ae
Nguyen The Hao <thehaohcm@yahoo.com.vn>
2017-07-03 22:40:13 +0700

    Hao fix some bugs & add some functions

--- GraphicProject/Form1.cs
+++ GraphicProject/Form1.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Collections;
 
-public delegate void drawCoordinates();
 namespace GraphicProject
 {
     public partial class Form1 : Form
