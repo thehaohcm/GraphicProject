e16e13e89a792df2a73435a1dab350791ccd2c60
Nguyễn Thế Hào <thehaohcm@yahoo.com.vn>
2017-07-04 08:35:30 +0000

    Upload New File

---
+++ GraphicProject/Point3D.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicProject
+{
+    class Point3D
+    {
+        int x, y, z;
+        public Point3D(int x,int y,int z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+
+            set
+            {
+                x = value;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+
+            set
+            {
+                y = value;
+            }
+        }
+
+        public int Z
+        {
+            get
+            {
+                return z;
+            }
+
+            set
+            {
+                z = value;
+            }
+        }
+    }
+}
